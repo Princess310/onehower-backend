453676d1fe18f10413d59ba5d5d498f69f739704
princess5535 <837478514@qq.com>
2017-06-19 17:34:08 +0800

    . add email and logger support

--- src/main/java/com/onehower/OnehowerApplication.java
+++ src/main/java/com/onehower/OnehowerApplication.java
@@ -1,10 +1,14 @@
 package com.onehower;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OnehowerApplication {
+	private static final Logger logger = LogManager
+			.getLogger(OnehowerApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnehowerApplication.class, args);
--- src/test/java/com/onehower/OnehowerApplicationTests.java
+++ src/test/java/com/onehower/OnehowerApplicationTests.java
@@ -1,16 +1,36 @@
 package com.onehower;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class OnehowerApplicationTests {
+	private static final Logger logger = LogManager
+			.getLogger(OnehowerApplicationTests.class);
+
+	@Autowired
+	private JavaMailSender mailSender;
 
 	@Test
-	public void contextLoads() {
+	public void testLogger() {
+		logger.info("Hello world");
 	}
 
+	@Test
+	public void testSendEmail() throws Exception {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setFrom("914092540@qq.com");
+		message.setTo("837478514@qq.com");
+		message.setSubject("主题：简单邮件");
+		message.setText("测试邮件内容");
+		mailSender.send(message);
+	}
 }
